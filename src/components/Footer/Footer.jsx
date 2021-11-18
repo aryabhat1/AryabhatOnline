e75764a7d4550b1d38c57bc860ab04a291252ae5
Aryabhat1 <sushant.pru@gmail.com>
2021-11-19 00:36:48 +0530

    My services as a freelancer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,143 @@
+import React from "react";
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-lg-10 mx-auto">
+              <div className="row">
+                <div className="col-6 col-lg-3">
+                  <h2>Company</h2>
+                  <ul>
+                    <li>
+                      <a href="#">About</a>
+                    </li>
+
+                    <li>
+                      <a href="#">About</a>
+                    </li>
+
+                    <li>
+                      <a href="#">About</a>
+                    </li>
+
+                    <li>
+                      <a href="#">About</a>
+                    </li>
+                  </ul>
+                </div>
+
+                <div className="col-6 col-lg-3">
+                  <h2>Our Team</h2>
+                  <ul>
+                    <li>
+                      <a href="#">About</a>
+                    </li>
+
+                    <li>
+                      <a href="#">About</a>
+                    </li>
+
+                    <li>
+                      <a href="#">About</a>
+                    </li>
+
+                    <li>
+                      <a href="#">About</a>
+                    </li>
+
+                    <li>
+                      <a href="#">About</a>
+                    </li>
+                  </ul>
+                </div>
+
+                <div className="col-6 col-lg-3">
+                  <h2>Our Locations</h2>
+                  <ul>
+                    <li>
+                      <a href="#">About</a>
+                    </li>
+
+                    <li>
+                      <a href="#">About</a>
+                    </li>
+
+                    <li>
+                      <a href="#">About</a>
+                    </li>
+
+                    <li>
+                      <a href="#">About</a>
+                    </li>
+
+                    <li>
+                      <a href="#">About</a>
+                    </li>
+                  </ul>
+                </div>
+
+                <div className="col-6 col-lg-3">
+                  <h2>Career</h2>
+                  <ul>
+                    <li>
+                      <a href="#">About</a>
+                    </li>
+
+                    <li>
+                      <a href="#">About</a>
+                    </li>
+
+                    <li>
+                      <a href="#">About</a>
+                    </li>
+
+                    <li>
+                      <a href="#">About</a>
+                    </li>
+
+                    <li>
+                      <a href="#">About</a>
+                    </li>
+                  </ul>
+                </div>
+
+                <div className="col-6 col-lg-3">
+                  <h2>Follow Us</h2>
+                  <div className="row">
+                    <div className="col-3 mx-auto">
+                      <a href="#">
+                        <i className="fab fa-instagram fontawesome-style"></i>
+                      </a>
+                    </div>
+                    <div className="col-3 mx-auto">
+                      <a href="#">
+                        <i className="fab fa-instagram fontawesome-style"></i>
+                      </a>
+                    </div>
+                    <div className="col-3 mx-auto">
+                      <a href="#">
+                        <i className="fab fa-instagram fontawesome-style"></i>
+                      </a>
+                    </div>
+
+                    <div className="col-3 mx-auto">
+                      <a href="#">
+                        <i className="fab fa-instagram fontawesome-style"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
